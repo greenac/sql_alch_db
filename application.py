6ae7e9bcada4e1ebe22452c9b7b21acfec01f52a
greenac <andre1226.dev@gmail.com>
2019-07-16 15:33:44 -0700

    Add SQLALCHEMY_DATABASE_URI

--- application.py
+++ application.py
@@ -4,9 +4,13 @@ import db.database as database
 #from db.database import db_url, DataBaseType
 from db.model import User
 
+
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = database.db_url(database.DataBaseType.main)
+
 db = SQLAlchemy(app)
+
 main_eng = database.engine(database.DataBaseType.main)
 connector = database.MainDbConnector()
 session = connector.new_session()
